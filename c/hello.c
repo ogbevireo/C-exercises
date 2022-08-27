9208c7b4cfdb1b5d0c031e31f77688c654dbab44
Ogbevire O., Obogrow <65548295+ogbevireo@users.noreply.github.com>
2022-08-27 03:32:08 +0100

    add3

--- c/hello.c
+++ c/hello.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int add3(int a, int b, int c){
+  int result = a + b + c;
+  return result;
+}
+
 int main() {
-  printf("Hello World\n");
+  int myNumber =0;
+  int other;
+  float areanumber = 4.5;
+  int result;
+
+  myNumber = 20;
+  other = 17;
+  result = myNumber + other / areanumber;
+
+  printf("Hello World %i %i %i %f\n",myNumber, other, result, areanumber);
+  printf("add3 = %i\n",add3(15,12,6));
 }
